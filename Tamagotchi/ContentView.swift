1f778933959f30f217a1aba3b366858d56625e0a
Zhenglong Wu <wu.z@etoncollege.org.uk>
2021-01-22 12:01:00 +0000

    Initial Commit

---
+++ Tamagotchi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tamagotchi
+//
+//  Created by Zhenglong Wu on 22/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tamagotchi/TamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiApp.swift
+//  Tamagotchi
+//
+//  Created by Zhenglong Wu on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
